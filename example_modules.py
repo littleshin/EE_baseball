5d59de80f8794acc2d8ed15467dd2a867291fe1b
Peter870512 <b05901070@ntu.edu.tw>
2020-04-19 01:56:31 +0800

    fix bugs and clean code

--- example_modules.py
+++ example_modules.py
@@ -6,12 +6,12 @@ import time
 def read_clip_mono(path):
     cap = cv2.VideoCapture(path)
     clip_buf=[]
-    a = 1.5
+    rate = 1.5 # enhance the pixel value
     while cap.isOpened():
         ret, frame = cap.read()
         if ret is False:
             break 
-        mono = frame[:,:,1] * a
+        mono = frame[:,:,1] * rate
         mono[mono > 255] = 255
         clip_buf.append(mono)
     return clip_buf
@@ -96,14 +96,13 @@ class MovingBallDetector(object):
         for kp in keypoints:
             points_xy.append( (kp.pt[0], kp.pt[1]) )
         return points_xy
+
     def choose_ball(self,ball_xy,ball_detect):
+        # if there are two or more detection of ball, choose the nearest of the last frame
         pre_ball = np.array(ball_detect[-1])
-        #print(pre_ball)
         balls = np.array(ball_xy)
         h,w = balls.shape
         pre_ball = np.repeat(pre_ball,h,axis=0)
-        #print(pre_ball)
-        #print('-----------')
         distance = np.sum( np.abs(balls - pre_ball),axis=0)
         pos = np.argmin(distance)
         balls = balls.astype(np.int32)
@@ -114,39 +113,39 @@ class MovingBallDetector(object):
         process_video = []
         hit_frame = []
         ball_detect = []
+        hit_time = 200
         for i, frame in enumerate(clip):
             fgmask = self.gen_differential_img(frame, mog=True)
-            fgmask = cv2.medianBlur(fgmask, 1)
+            fgmask = cv2.medianBlur(fgmask, 1) # reduce the noise
             fgmask = fgmask[:,50:-130] # size : [540,540,3]
             blob, keypoints = self.draw_blob_detected_ball_on_img(fgmask)
-            if keypoints != [] and i > 200:
+            if keypoints != [] and i > hit_time:
                 ball_xy = self.get_ball_xy(keypoints)
                 if len(ball_xy) > 1:
-                    #print(ball_detect)
                     ball_xy = self.choose_ball(ball_xy,ball_detect)
                 ball_detect.append(ball_xy)
 
                 if len(ball_detect) > 2:
                     hit = self.check_hit(ball_detect)
                     if hit:
-                        print(i)
                         hit_frame.append(i)
             process_video.append(blob)
-        #print(ball_detect)
         return process_video, hit_frame
+
     def check_hit(self,ball_detect):
         ball_1 = ball_detect[-3][0]
         ball_2 = ball_detect[-2][0]
         ball_3 = ball_detect[-1][0]
-        hit = False
-        if ball_3[0] < ball_2[0] and ball_3[1] < ball_2[1]:
-            if ball_2[0] < ball_1[0] and ball_2[1] < ball_1[1]:
-                hit = True
+        ball_is_going_left_up = (ball_3[0] < ball_2[0] and ball_3[1] < ball_2[1]) \
+            and (ball_2[0] < ball_1[0] and ball_2[1] < ball_1[1])
+        ball_is_going_left_down = (ball_3[0] > ball_2[0] and ball_3[1] < ball_2[1]) \
+            and (ball_2[0] > ball_1[0] and ball_2[1] < ball_1[1])
+        hit = ball_is_going_left_up or ball_is_going_left_down
         return hit
 
         
 
-def run_param_for_bgs():
+def run_param_for_bgs(pause_mode):
     path_1 = ('./material/LHB_240FPS/Tang_toss_0101.avi')
     path_2 = ('./material/LHB_240FPS/Lin_toss_1227 (2).avi')
     clip_buf_1 = read_clip_mono(path_1)
@@ -161,42 +160,40 @@ def run_param_for_bgs():
     print("hist = "+str(hist), "thres = "+str(thres))
     ball_detector_1 = MovingBallDetector(clip_buf_1[0],hist=hist, thres=thres, kr=3)
     ball_detector_2 = MovingBallDetector(clip_buf_2[0],hist=hist, thres=thres, kr=3)
-    video_1, hit_frame_1 = ball_detector_1.get_process_video(clip_buf_1[0:frame_total_1])
-    video_2, hit_frame_2 = ball_detector_2.get_process_video(clip_buf_2[0:frame_total_2])
-    print(hit_frame_1)
-    print(hit_frame_2)
+    video_1, hit_frame_1 = ball_detector_1.get_process_video(clip_buf_1)
+    video_2, hit_frame_2 = ball_detector_2.get_process_video(clip_buf_2)
+
+    # print the hit frame of the video_n
+    print("The hit frames of video 1: ", hit_frame_1)
+    print("The hit frames of video 2: ", hit_frame_2)
     
+    # Calculate the difference of two video and convert the process video to normal video
     if hit_frame_1[0] > hit_frame_2[0]:
         different = hit_frame_1[0] - hit_frame_2[0]
+        video_1 = read_clip_rgb(path_1)
         video_1 = video_1[different-1:]
+        video_2 = read_clip_rgb(path_2)
     else:
         different = hit_frame_2[0] - hit_frame_1[0]
+        video_2 = read_clip_rgb(path_2)
         video_2 = video_2[different-1:]
-    
-    less = min(len(video_1),len(video_2))
-    
-    for i in range(less):#len(video_1)):
+        video_1 = read_clip_rgb(path_1)
+
+    less = min(len(video_1), len(video_2))
+    # playing the video 
+    for i in range(less):
         hmerge = np.hstack((video_1[i],video_2[i]))
+        cv2.namedWindow("Two video merge",0)
+        cv2.resizeWindow("Two video merge", hmerge.shape[1], hmerge.shape[0])
         cv2.imshow('Two video merge', hmerge)       
-        #cv2.waitKey(40)
-        
-        if i in hit_frame_1 or i in hit_frame_2:
-            print(i)
-            cv2.waitKey(0)
+        if pause_mode:
+            if i in hit_frame_1 or i in hit_frame_2:
+                cv2.waitKey(0)
+            else:
+                cv2.waitKey(40)
         else:
             cv2.waitKey(40)
-        '''
-        if i == hit_frame_1[0] - hit_theshold:
-            print(i)
-            cv2.waitKey(0)
-        elif i == hit_frame_2[0] - hit_theshold:
-            print(i)
-            cv2.waitKey(0)
-        
-        else:
-            cv2.waitKey(50)
-        '''
-    #ball_detector.demo_video(clip_buf_1[0:frame_total])
-    #print("ms per frame: "+str((time.time()-t0)*1000/frame_total))    
-
-run_param_for_bgs()
+  
+# pause_mode = True : the video will pause at the hitting frame
+# pause_mode = False : the video will not be pause
+run_param_for_bgs(pause_mode=False)
